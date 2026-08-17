587e447e62071d1912de6972b39acc94213953f8
everafterinvites <contact.everafterinvites@gmail.com>
2026-08-17 20:08:11 +0000

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Slot-machine-save-the-date-/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
